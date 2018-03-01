6dce1b5209e0a32938049607e194bbfe55850b91
Oleh Yarkovyi <oleh.yarkovyi@gmail.com>
2018-03-01 14:04:53 +0100

    commit 1

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Hello, ololo!',
+            'author': 'Oleh Yarlovyi',
+            'create_date': '28-02-2018'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Hello, lololo!',
+            'author': 'Kostyantyn Molchanov',
+            'create_date': '28-02-2018'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Shut up, Flooders!',
+            'author': 'Admin',
+            'create_date': '28-02-2018'
+        }
+    ]
+    return(articles)

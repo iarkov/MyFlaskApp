76ca401d2938cc11692d130983114ba8d5729447
Oleh Yarkovyi <oleh.yarkovyi@gmail.com>
2018-03-04 15:51:34 +0100

    add 'add_article func'

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField
 from wtforms import PasswordField, validators
 from passlib.hash import sha256_crypt
+from functools import wraps
 
 class RegisterForm(Form):
     name = StringField('Name', [validators.Length(min=6, max=30)])
@@ -16,6 +17,11 @@ class RegisterForm(Form):
         validators.EqualTo('confirm', message='Passwords do not match')
     ])
     confirm = PasswordField('Confirm Password')
+
+class ArticleForm(Form):
+    title = StringField('Title', [validators.Length(min=6, max=255)])
+    body = TextAreaField('Username', [validators.Length(min=30)])
+
 
 
 app = Flask(__name__)
@@ -118,8 +124,20 @@ def login():
 
     return render_template('login.html')
 
+# Check if user logged in
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Unauthorized. Please log in', 'danger')
+            return redirect(url_for('login'))
+    return wrap
+    
 # Logout
 @app.route('/logout')
+@is_logged_in
 def logout():
     session.clear()
     flash('You are logged out', 'success')
@@ -127,8 +145,33 @@ def logout():
 
 # Dashboard
 @app.route('/dashboard')
+@is_logged_in
 def dashboard():
     return render_template('dashboard.html')
+
+# Add Article Route
+@app.route('/add_article', methods=['GET', 'POST'])
+@is_logged_in
+def add_article():
+    form = ArticleForm(request.form)
+    if request.method == 'POST' and form.validate():
+        title = form.title.data
+        body = form.body.data
+
+        cur = mysql.connection.cursor()
+
+        cur.execute("INSERT INTO articles(title, body, author) VALUES(%s, %s, %s)", (title, body, session['username']))
+
+        mysql.connection.commit()
+
+        cur.close()
+
+        flash('Article created', 'success')
+
+        return redirect(url_for('dashboard'))
+
+    return render_template('add_article', form=form)
+
 
 if __name__ == '__main__':
     app.secret_key = 'secret123'
